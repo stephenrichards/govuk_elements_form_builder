89d6985cd352f5bcfde7290c29db76e8204c8751
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-03-17 07:51:08 +0000

    initial specs

---
+++ spec/govuk_elements_form_builder_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe GovukElementsFormBuilder do
+  it "should have a version" do
+    expect(GovukElementsFormBuilder::VERSION).to eq("0.0.1")
+  end
+end

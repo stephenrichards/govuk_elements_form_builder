a4a9de92cc2d3aa2f5e52b70912c246bb8ba5633
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2016-03-18 15:15:15 +0000

    Add builder method to generate labelled input
    Test method using activemodel Person example in the spec/dummy
    Rails app.

--- lib/govuk_elements_form_builder/form_builder.rb
+++ lib/govuk_elements_form_builder/form_builder.rb
@@ -3,9 +3,19 @@ module GovukElementsFormBuilder
     def text_field(name,*arg)
       @template.content_tag :div, class: 'form-group' do
         options = arg.extract_options!
+
         label_class = ["form-label"]
-        label_class << options[:label][:class]] if options[:label][:class].present?
-        label(name, class: label_class) + super
+        if options[:label].present?
+          label_class << options[:label][:class] if options[:label][:class].present?
+        end
+
+        text_field_class = ["form-control"]
+        if options[:class].present?
+          text_field_class << options[:class] if options[:class].present?
+        end
+        options[:class] = text_field_class
+
+        label(name, class: label_class) + super(name, options.except(:label))
       end
     end
   end

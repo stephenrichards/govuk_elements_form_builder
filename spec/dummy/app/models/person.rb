d3fe079a5c4a3e44b7fb5f9e9c56e0cc2af37fad
robmckinnon <rob@movingflow>
2016-03-24 09:09:06 +0000

    Render hint text in text field label
    When hint text is configured in locales file for a given field
    render the hint text in a `span` inside the `label` when
    `text_field` helper method is used.
    
    Hint markup defined in GOV.UK elements guide here:
    https://govuk-elements.herokuapp.com/form-elements/#form-hint-text

--- spec/dummy/app/models/person.rb
+++ spec/dummy/app/models/person.rb
@@ -3,4 +3,7 @@ class Person
 
   attr_accessor :name
   validates_presence_of :name
+
+  attr_accessor :ni_number
+
 end
--- spec/lib/govuk_elements_form_builder/form_builder_spec.rb
+++ spec/lib/govuk_elements_form_builder/form_builder_spec.rb
@@ -13,15 +13,41 @@ RSpec.describe GovukElementsFormBuilder::FormBuilder do
   let(:resource)  { Person.new }
   let(:builder) { described_class.new :person, resource, helper, {} }
 
-  describe '#text_field' do
-    it 'outputs correct markup' do
-      output = builder.text_field :name
-      expect(output).to eq '<div class="form-group">' +
-        '<label class="form-label" for="person_name">Full name</label>' +
-        '<input class="form-control" type="text" name="person[name]" id="person_name" />' +
-        '</div>'
-    end
+  def expect_equal output, expected
+    split_output = output.split("<").join("\n<").split(">").join(">\n").squeeze("\n").strip + '>'
+    split_expected = expected.join("\n")
+    expect(split_output).to eq split_expected
   end
 
+  describe '#text_field' do
+    it 'outputs label and input wrapped in div' do
+      output = builder.text_field :name
+      expect_equal output, [
+        '<div class="form-group">',
+        '<label class="form-label" for="person_name">',
+        'Full name',
+        '</label>',
+        '<input class="form-control" type="text" name="person[name]" id="person_name" />',
+        '</div>'
+      ]
+    end
+
+    context 'when hint text provided' do
+      it 'outputs hint text in span inside label' do
+        output = builder.text_field :ni_number
+        expect_equal output, [
+          '<div class="form-group">',
+          '<label class="form-label" for="person_ni_number">',
+          'National Insurance number',
+          '<span class="form-hint">',
+          'It’ll be on your last payslip. For example, JH 21 90 0A.',
+          '</span>',
+          '</label>',
+          '<input class="form-control" type="text" name="person[ni_number]" id="person_ni_number" />',
+          '</div>'
+        ]
+      end
+    end
+  end
 
 end
